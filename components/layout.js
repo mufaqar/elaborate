73be6776d8057159fa13690f434f84a5d4f32c92
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-05 17:43:29 +0500

    padding

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     return (
         <main className='min-h-screen relative grid items-center'>
             <Header />
-            <section className='md:fixed md:top-[7rem] md:bottom-auto md:left-0 md:right-0 grid items-center'>
+            <section className='md:fixed md:top-[7rem] md:bottom-auto md:left-0 md:right-0 grid items-center md:pt-0 pt-[112px] md:pb-0 pb-[45px]'>
                 {children}
             </section>
             <Footer />
