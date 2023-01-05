bf0d2859e61179f2d13222d091c4efe730bf3bde
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-05 17:34:48 +0500

    position

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className='border-y border-black fixed left-0 right-0 bottom-0 bg-zinc-500 py-2'>
+        <footer className='border-y border-black md:fixed sticky left-0 right-0 bottom-0 bg-zinc-500 py-2'>
             <div className="overflow-x-hidden">
                 <div className="animate-marquee whitespace-nowrap">
                     <span className="text-base text-white mx-4">Marquee Item 1</span>
