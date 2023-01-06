3a35d9f40870e4b6f6fe869566fd19e533a9ce01
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-06 14:55:04 +0500

    hover

--- components/newsBox.js
+++ components/newsBox.js
@@ -5,16 +5,16 @@ import img_url from '../public/images/dummy.jpg';
 
 export default function NewsBox() {
   return (
-    <div className="min-h-full md:border-l border-black relative">
-      {/* <iframe className="w-full aspect-[9/12] object-cover" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+    <div className="min-h-full md:border-l border-black relative group">     
       <div className="h-full">
-        <Image src={img_url} alt="" className="object-fill min-h-full" />
+         <iframe className="w-full h-full aspect-[9/12] object-cover" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+        {/* <Image src={img_url} alt="" className="object-fill min-h-full" /> */}
       </div>
-      <div className="flex px-3 justify-between h-[36px] bg-white absolute left-0 right-0 bottom-6">
-        <Link href="/news" className="text-xs leading-[16px] py-2 uppercase">
+      <div className="flex px-3 justify-between h-[36px] bg-white absolute left-0 right-0 md:-bottom-6 md:group-hover:bottom-6 -bottom-10 group-hover:bottom-0">
+        <Link href="/news" className="text-xs leading-[16px] py-2 uppercase text-blue-600">
           Victoria, Australia
         </Link>
-        <p className="text-xs leading-[16px] border-l border-black/80 px-2 py-2 uppercase">
+        <p className="text-xs leading-[16px] border-l border-black/80 px-2 py-2 uppercase text-red-600 ">
           12:31 PM
         </p>
       </div>
