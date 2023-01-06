dda1aee38feae8989f027387e079f687ab622da4
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-06 19:18:34 +0500

    footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import LiveScroll from './liveScroll';
 
 export default function Footer() {
     return (
-        <footer className='border-y border-black fixed left-0 right-0 bottom-0  '>
+        <footer className='fixed left-0 right-0 bottom-0'>
             <LiveScroll Custom_bg="bg-[#7B7B7B]" Text_color="text-white" />
         </footer>
     )
