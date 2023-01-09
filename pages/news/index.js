2e3c57a0c0dc4c032e05150a9b8f67fa1c9ee717
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-09 18:32:23 +0500

    height

--- pages/news/index.js
+++ pages/news/index.js
@@ -5,7 +5,7 @@ import img_url from '../../public/images/dummy.jpg';
 
 export default function Slug() {
   return (
-    <section className="relative h-full">
+    <section className="relative h-sccreen">
       {/* <iframe className="w-full md:aspect-[12/4] aspect-[9/12] object-cover" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
       <div className='relative h-[90vh]'>
         <Image src={img_url} alt="" className='object-cover h-full' />
