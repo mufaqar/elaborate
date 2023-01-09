da6f8ab6baa7b745444ae70942df92d80f08a1a7
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-09 16:35:29 +0500

    header fixed

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     return (
         <main className='relative'>
             <Header />
-            <section className='fixed 2xl:top-[5rem] xl-[4.9rem] md:top-[4.3rem] sm:top-[6rem] top-[4.4rem] bottom-0 left-0 right-0 '>
+            <section className='fixed 2xl:top-[5rem] xl-[4.9rem] md:top-[4.3rem] sm:top-[6rem] top-[4.4rem] bottom-0 left-0 right-0 -z-10'>
                 {children}
             </section>
             <Footer />
