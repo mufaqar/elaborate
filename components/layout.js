3ce95326d77bb83138b39ffe4e8b5876345ba64f
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-09 16:20:41 +0500

    single page updated

--- components/layout.js
+++ components/layout.js
@@ -4,9 +4,9 @@ import Header from './header';
 
 export default function Layout({ children }) {
     return (
-        <main className='min-h-screen relative grid items-center'>
+        <main className='relative'>
             <Header />
-            <section className='fixed 2xl:top-[5rem] xl-[5rem] md:top-[4.5rem] sm:top-[6rem] top-[4.4rem] bottom-0 left-0 right-0 '>
+            <section className='fixed 2xl:top-[5rem] xl-[4.9rem] md:top-[4.3rem] sm:top-[6rem] top-[4.4rem] bottom-0 left-0 right-0 '>
                 {children}
             </section>
             <Footer />
--- pages/news/index.js
+++ pages/news/index.js
@@ -5,16 +5,16 @@ import img_url from '../../public/images/dummy.jpg';
 
 export default function Slug() {
   return (
-    <section className="md:pt-0 pt-[75px] md:pb-0 pb-[45px]">
+    <section className="relative h-full">
       {/* <iframe className="w-full md:aspect-[12/4] aspect-[9/12] object-cover" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-      <div className='md:h-[78vh] h-[97vh]'>
-        <Image src={img_url} alt="" className='h-full object-cover' />
+      <div className='relative h-[90vh]'>
+        <Image src={img_url} alt="" className='object-cover h-full' />
       </div>
-      <div className='flex px-3 '>
-        <p className='text-lg leading-7 px-2 py-3 uppercase NeueHaasRoman'>
+      <div className='flex px-3 absolute bottom-0 left-0 right-0 bg-white'>
+        <p className='text-lg leading-7 px-2 pt-3 pb-7 uppercase NeueHaasRoman'>
           Victoria, Australia
         </p>
-        <p className='text-lg leading-7 border-l border-black/80 px-2 py-3 uppercase NeueHaasRoman'>
+        <p className='text-lg leading-7 border-l border-black/80 px-2 pt-3 pb-7 uppercase NeueHaasRoman'>
           12:31 PM
         </p>
       </div>
