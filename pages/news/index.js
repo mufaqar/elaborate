a248241cd6598c1d54161c3ebe6235360497845d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-10 16:03:52 +0500

    single video

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,15 +6,16 @@ import img_url from '../../public/images/dummy.jpg';
 export default function Slug() {
   return (
     <section className="relative h-sccreen">
-      {/* <iframe className="w-full md:aspect-[12/4] aspect-[9/12] object-cover" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
       <div className='relative h-[90vh]'>
-        <Image src={img_url} alt="" className='object-cover h-full' />
+        <iframe
+          src={`https://www.youtube.com/embed/DbLMSFvB2Og?controls=1&autoplay=1&mute=1&playsinline=1&loop=1`}
+        ></iframe>
       </div>
       <div className='flex px-3 absolute bottom-0 left-0 right-0 bg-white'>
-        <p className='text-xs leading-4 px-2 pt-2.5 pb-9 uppercase NeueHaasRoman'>
+        <p className='text-xs leading-4 px-2 pt-3 pb-3 uppercase NeueHaasRoman'>
           Victoria, Australia
         </p>
-        <p className='text-xs leading-4 border-l border-black/80 px-2 pt-2.5 pb-9 uppercase NeueHaasRoman'>
+        <p className='text-xs leading-4 border-l border-black/80 px-2 pt-2.5 pb-2.5 uppercase NeueHaasRoman'>
           12:31 PM
         </p>
       </div>
