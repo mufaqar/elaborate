dac66103f67e9321da9c64dd93aa4493faab5767
Mufaqar Islam <mufaqar@gmail.com>
2023-01-10 18:08:14 +0500

    styling

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,25 @@
 export default function About() {
   return (
-    <section className="py-[90px]">
+    <section className="py-[120px]">
       <div className="container mx-auto ">
-        <h2>About Elaborate Links</h2>
-        <p>Details </p>
+        <h2> Welcome To Elaborate Links</h2>
+        <br />
+        <p className="mb-4">
+          Elaborate Links is a Professional live Steaming Platform. Here we will
+          provide you only interesting content, which you will like very much.
+          We're dedicated to providing you the best of live Steaming, with a
+          focus on dependability and sport live streaming. We're working to turn
+          our passion for live Steaming into a booming online website. We hope
+          you enjoy our live Steaming as much as we enjoy offering them to you.
+        </p>
+        <p className="mb-4">
+          I will keep posting more important posts on my Website for all of you.
+          Please give your support and love.
+        </p>
+
+        <p className="mb-4">Thanks For Visiting Our Site</p>
+        <br />
+        <p className="mb-4">Have a nice day!</p>
       </div>
     </section>
   );
--- pages/stories.js
+++ pages/stories.js
@@ -1,9 +1,25 @@
 export default function Story() {
   return (
-    <section className="py-[90px]">
+    <section className="py-[120px]">
       <div className="container mx-auto ">
-        <h2>Our Story</h2>
-        <p>This is About us Page</p>
+        <h2 className="text-2xl mb-4"> Elaborate Links Story</h2>
+
+        <p className="mb-4">
+          Elaborate Links is a Professional live Steaming Platform. Here we will
+          provide you only interesting content, which you will like very much.
+          We're dedicated to providing you the best of live Steaming, with a
+          focus on dependability and sport live streaming. We're working to turn
+          our passion for live Steaming into a booming online website. We hope
+          you enjoy our live Steaming as much as we enjoy offering them to you.
+        </p>
+        <p className="mb-4">
+          I will keep posting more important posts on my Website for all of you.
+          Please give your support and love.
+        </p>
+
+        <p className="mb-4">Thanks For Visiting Our Site</p>
+        <br />
+        <p className="mb-4">Have a nice day!</p>
       </div>
     </section>
   );
