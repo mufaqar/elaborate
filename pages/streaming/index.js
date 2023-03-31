2466410b2289188fbbe9b93938202f1fb4fb1b21
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-03-31 15:39:49 +0500

    updated

--- pages/streaming/index.js
+++ pages/streaming/index.js
@@ -20,15 +20,15 @@ export default function Slug({ url }) {
         ></iframe>
         <div className='absolute bottom-24 left-5 '>
           <Image src={Cam} alt="" />
-          <p className="mt-5 text-xs leading-4 px-2 pt-3 pb-3 uppercase NeueHaasRoman bg-white">
+          <p className="mt-5 text-xs leading-4 px-2 pt-3 pb-3 uppercase NeueHaasRoman bg-[#F2F2F2]">
             {title}<br/>
             {s}
           </p>
         </div>
-        <div className={`bg-[#F2F2F2] grid md:grid-cols-4 grid-cols-1 absolute bottom-6 left-0 right-0 `}>
+        <div className={` grid md:grid-cols-4 grid-cols-1 absolute bottom-6 left-0 right-0 `}>
           {videoData.map((item, i) => (
             <div key={i}>
-              <div className="flex">
+              <div className={`flex bg-[#F2F2F2] h-full`}>
                 <Link href={`/streaming?streaming=${item.url}&title=${item.title}`}
                   className="text-xs leading-4 px-2 pt-3 pb-3 uppercase NeueHaasRoman border-l border-[#707070] [&:nth-child(1)>p]:border-l-0">
                   {item.title}
