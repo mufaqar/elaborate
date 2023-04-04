5fd68d182238eccee2b7df134a4252cf80fb4017
Mufaqar <mufaqar@gmail.com>
2023-04-04 23:11:11 +0500

    updaetd

--- components/footer.js
+++ components/footer.js
@@ -27,7 +27,7 @@ export default function Footer() {
                   link == item.url && 'text-[#F2F2F2]'
                 }`}
               >
-                {item.title}
+                <span className="font-bold"> {item.title}</span> - {item.title}
               </Link>
               <p
                 className={`text-xs leading-4 px-2 pt-[8px] pb-[8px] uppercase NeueHaasRoman border-l border-[#707070] ${
--- public/videos.data.js
+++ public/videos.data.js
@@ -1,18 +1,22 @@
 export const videoData = [
   {
     url: 'DbLMSFvB2Og',
-    title: 'THE CASTLE COURSE - ST ANDREWS, SCOTLAND',
+    title: 'THE CASTLE COURSE',
+    title2: 'ST ANDREWS, SCOTLAND',
   },
   {
     url: 'gauf1M1CvFo',
-    title: 'PEBBLE BEACH GOLF LINKS - PEBBLE BEACH, CA, USA',
+    title: 'PEBBLE BEACH GOLF LINKS ',
+    title2: '- PEBBLE BEACH, CA, USA',
   },
   {
     url: 'w9ItD-QixNE',
-    title: 'BANDON DUNES GC BANDON, OREGON, USA',
+    title: 'BANDON DUNES GC ',
+    title2: 'BANDON, OREGON, USA',
   },
   {
     url: 'gauf1M1CvFoo',
-    title: 'ROYAL PORTHCAWL - PORTHCAWL, WALES, UK',
+    title: 'ROYAL PORTHCAWL',
+    title2: 'PORTHCAWL, WALES, UK',
   },
 ];
