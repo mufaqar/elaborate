75ea0ca8bb0454b14461ba9360a28e2934c74131
Mufaqar <mufaqar@gmail.com>
2023-05-05 20:47:38 +0500

    Footer space

--- components/footer.js
+++ components/footer.js
@@ -31,32 +31,32 @@ export default function Footer({ url }) {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <footer className={`fixed left-0 right-0 bottom-0 `}>
       <div className="absolute md:bottom-24 bottom-24 left-4 md:right-auto right-4 max-w-[345px] mx-auto">
         <div className={`${link == url ? 'hidden' : 'block'} mx-auto`}>
-          <Image src={Cam} alt="" className='mx-auto' />
+          <Image src={Cam} alt="" className="mx-auto" />
           <div className="md:flex inline-block w-full items-center my-3 justify-between">
             <div className="flex border border-white mx-auto ">
               <p className=" text-xs leading-4 px-[6px] pr-[16px] NeueHaasRoman bg-[#F2F2F2] text-black min-w-[212px] w-3/4 ">
@@ -69,24 +69,17 @@ export default function Footer({ url }) {
           </div>
         </div>
         <div className="flex items-center border border-black/50">
-          <div className='bg-[#EBFD00] md:p-3 md:h-9 md:w-9 h-8 w-7 p-2 cursor-pointer'
+          <div
+            className="bg-[#EBFD00] md:p-3 md:h-9 md:w-9 h-8 w-7 p-2 cursor-pointer"
             onClick={() => {
               setAudio(!audio);
             }}
           >
             {audio ? (
-              <Image
-                src={Stop}
-                alt=""
-                className="object-contain"
-              />) :
-              (
-                <Image
-                  src={Play}
-                  alt=""
-                  className="object-contain"
-                />
-              )}
+              <Image src={Stop} alt="" className="object-contain" />
+            ) : (
+              <Image src={Play} alt="" className="object-contain" />
+            )}
           </div>
           <p className="text-sm leading-4 pt-2 pb-2 md:px-[10px] pl-[6px] pr-0 NeueHaasRoman bg-[#F2F2F2] md:h-9 h-8 w-full">
             Field Recordings from Augusta National Golf Club
@@ -95,28 +88,35 @@ export default function Footer({ url }) {
       </div>
 
       <div
-        className={`border-t-[13px] border-[#EBFD00]  ${link == url ? 'block' : 'md:block hidden'} ${router.pathname == '/' && 'md:block hidden'}`}
+        className={`border-t-[13px] border-[#EBFD00]  ${
+          link == url ? 'block' : 'md:block hidden'
+        } ${router.pathname == '/' && 'md:block hidden'}`}
       >
-        <div
-        >
+        <div>
           <Slider {...settings}>
             {videoData.map((item, i) => (
               <div key={i} className="bg-[#F2F2F2]">
                 <div
-                  className={`flex h-[47px] items-center justify-between ${link == item.url && 'bg-[#414141]'
-                    }`}
+                  className={`flex h-[25px] items-center justify-between ${
+                    link == item.url && 'bg-[#414141]'
+                  }`}
                 >
                   <Link
                     href={`/streaming?streaming=${item.url}&title=${item.title}&title2=${item.title2}`}
                     className={`text-xs leading-4 px-2 pt-[6px] pb-[6px] uppercase h-full items-center inline-flex border-l border-[#707070] [&:nth-child(1)>p]:border-l-0
                 ${link == item.url && 'text-[#F2F2F2]'}`}
                   >
-                    <span className="HelveticaNowText_Bold"> {item.title}</span> -{' '}
-                    <span className="HelveticaNowText_Regular italic"> {item.title2}</span>
+                    <span className="HelveticaNowText_Bold"> {item.title}</span>{' '}
+                    -{' '}
+                    <span className="HelveticaNowText_Regular italic">
+                      {' '}
+                      {item.title2}
+                    </span>
                   </Link>
                   <p
-                    className={`text-xs leading-4 px-2 pt-[6px] pb-[6px] uppercase NeueHaasRoman h-full items-center inline-flex border-l border-[#707070] ${link == item.url && 'text-[#EBFF00]'
-                      }`}
+                    className={`text-xs leading-4 px-2 pt-[6px] pb-[6px] uppercase NeueHaasRoman h-full items-center inline-flex border-l border-[#707070] ${
+                      link == item.url && 'text-[#EBFF00]'
+                    }`}
                   >
                     {s}
                   </p>
